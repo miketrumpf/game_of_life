2a5f8d29754f2a8642cfdd61364716957896132d
Michael Trumpf <miketrumpf@gmail.com>
2015-08-17 21:34:13 -0400

    added sessions controller and routes.  helper methods in application controller.  added a few gems.

--- game_of_life/config/routes.rb
+++ game_of_life/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :goals
   resources :users
+
+
+  get '/sessions/new' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
